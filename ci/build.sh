1d5fec644be43cab9ba002ea24110fb30a74b6e3
Sai Boorlagadda <sboorlagadda@pivotal.io>
2016-05-20 23:20:21 -0700

    added versioning to jar

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,9 @@ export TERM=${TERM:-dumb}
 
 set -e
 
+VERSION=$(cat configserver-version/version)
+
 pushd easy-config-server
-./gradlew bootRepackage
+./gradlew build -Pversion=$VERSION
 popd
-cp easy-config-server/build/libs/configserver.jar builds/
+cp easy-config-server/build/libs/configserver*.jar builds/
